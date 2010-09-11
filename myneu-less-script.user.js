93ec771aab8d955704bd57b0a81fee9b0873a1c2
Tim McCormack <cortex@brainonfire.net>
2010-09-11 13:43:01 -0400

    First version, just replaces OpenNEU() calls

---
+++ myneu-less-script.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name           MyNEU javascript remover
+// @namespace      tag:brainonfire.net,2008-07-27:myneu-less-script
+// @description    Replace Javascript popup links with real links & target=_blank
+// @include        *myneu*
+// @version        0.1
+// @changelog      First version, just replaces OpenNEU() calls
+// ==/UserScript==
+
+
+/* From http://wiki.greasespot.net/Code_snippets */
+function $xpath(p, context)
+{
+	if(!context)
+		context = document;
+	var i;
+	var arr = [];
+	var xpr = document.evaluate(p, context, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+	for(i = 0; item = xpr.snapshotItem(i); i++)
+		arr.push(item);
+	return arr;
+}
+
+
+//modules
+function generic_location()
+{
+	var tabs = $xpath('//a[starts-with(@href, "javascript:OpenNEU(\'http:")]');
+	tabs.forEach(function(link)
+	{
+		var oldHref = link.getAttribute('href');
+		var newHref = oldHref.replace(/^javascript:OpenNEU\('http:(([^']|\\')+)'\);?$/, '$1');
+		if(oldHref !== newHref)
+		{
+			link.setAttribute('href', newHref);
+			link.setAttribute('target', '_blank');
+		}
+	});
+}
+
+//TODO: Put in modules here, to be called in different URL contexts
+
+//do different pages and sections
+
+generic_location();
+
+//TODO: you can put in the URL-switched module calls here
